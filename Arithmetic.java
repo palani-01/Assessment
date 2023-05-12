9d13f1e54b8d6c982fab7100af3ffa4f22be7fca
Palani <spktceit@gmail.com>
2023-05-12 11:13:27 +0530

    fourth commit

--- Arithmetic.java
+++ Arithmetic.java
@@ -1,18 +1,43 @@
-class Arithmetic{
+class Arithmetic {
 
-    public int add(int a, int b) {
+    private int first_value;
+    private int second_value;
+    public int getFirst_value() {
+        return first_value;
+    }
+
+    public void setFirst_value(int first_value) {
+        this.first_value = first_value;
+    }
+
+    public int getSecond_value() {
+        return second_value;
+    }
+
+    public void setSecond_value(int second_value) {
+        this.second_value = second_value;
+    }
+
+    
+
+    public Arithmetic(int first_value,int second_value){
+        this.first_value = first_value;
+        this.second_value = second_value;
+    }
+
+    public final int add(int a, int b) {
         return a + b;
     }
 
-    public int sub(int a, int b) {
+    public final int sub(int a, int b) {
         return a - b;
     }
 
-    public int mul(int a, int b) {
+    public final int mul(int a, int b) {
         return a * b;
     }
 
-    public int div(int a, int b) {
+    public final int div(int a, int b) {
         return a / b;
     }
 
--- arithmeticCalculator.java
+++ arithmeticCalculator.java
@@ -2,31 +2,30 @@ import java.util.*;
 
 public class arithmeticCalculator {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the first value");
-        int a = sc.nextInt();
-        System.out.println("Enter the second value");
-        int b = sc.nextInt();
-        sc.nextLine();
+           Scanner sc = new Scanner(System.in);
+        // System.out.println("Enter the first value");
+        // int a = sc.nextInt();
+        // System.out.println("Enter the second value");
+        // int b = sc.nextInt();
+        // sc.nextLine();
         System.out.println("Enter the operator\n\n1.Add(+)\n2.Sub(-)\n3.Multiply(*)\n4.Divide(/)");
 
-        
-
         String operator = sc.nextLine();
-        Arithmetic obj = new Arithmetic();
+        Arithmetic obj = new Arithmetic(5,5);
+        obj.setSecond_value(6);
         switch (operator) {
             case "+":
-                System.out.println("Sum of a & b: "+ obj.add(a,b));
+                System.out.println("Sum of a & b: " + obj.add(obj.getFirst_value(), obj.getSecond_value()));
                 break;
             case "-":
 
-                System.out.println("Difference of a & b: " + obj.sub(a,b));
+                System.out.println("Difference of a & b: " + obj.sub(obj.getFirst_value(), obj.getSecond_value()));
                 break;
             case "*":
-                System.out.println("Product of a & b: " + obj.mul(a,b));
+                System.out.println("Product of a & b: " + obj.mul(obj.getFirst_value(), obj.getSecond_value()));
                 break;
             case "/":
-                System.out.println("Quotient of a by b: " + obj.div(a,b));
+                System.out.println("Quotient of a by b: " + obj.div(obj.getFirst_value(), obj.getSecond_value()));
                 break;
             default:
                 System.out.println("invalid option");
